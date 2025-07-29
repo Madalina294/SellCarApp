3220cb973d8f72912f5948caa0ea8a55a3f58ccd
Madalina294 <puicanmadalinamaria@gmail.com>
2025-07-29 21:32:18 +0300

    Added routing to the frontend

--- sellCar_angular/src/app/app.routes.ts
+++ sellCar_angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './auth/auth-components/login/login.component';
+import {SingupComponent} from './auth/auth-components/singup/singup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login", component:LoginComponent},
+  {path:"signup", component: SingupComponent},
+  {path:"admin", loadChildren:() =>
+      import("./modules/admin/admin.module").then(e => e.AdminModule)},
+  {path:"customer", loadChildren:() =>
+      import("./modules/customer/customer.module").then(e => e.CustomerModule)}
+];
