0ef95bc485db8638eaf39d52eac2c5812066ba17
Madalina294 <puicanmadalinamaria@gmail.com>
2025-08-11 00:25:59 +0300

    Added Log In form design and validation

--- sellCar_angular/src/app/auth/auth-components/login/login.component.ts
+++ sellCar_angular/src/app/auth/auth-components/login/login.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzColDirective, NzRowDirective} from "ng-zorro-antd/grid";
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from "ng-zorro-antd/form";
+import {NzInputDirective} from "ng-zorro-antd/input";
+import {NzSpinComponent} from "ng-zorro-antd/spin";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+    imports: [
+        FormsModule,
+        NgIf,
+        NzButtonComponent,
+        NzColDirective,
+        NzFormControlComponent,
+        NzFormDirective,
+        NzFormItemComponent,
+        NzInputDirective,
+        NzRowDirective,
+        NzSpinComponent,
+        ReactiveFormsModule,
+        RouterLink
+    ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  isSpinning: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/)
+      ]],
+    });
+  }
+
+  login(){
+    if (this.loginForm.invalid) {
+      Object.values(this.loginForm.controls).forEach(control => control.markAsDirty());
+      this.loginForm.updateValueAndValidity();
+      return;
+    }
+    console.log(this.loginForm.value);
+  }
 }
