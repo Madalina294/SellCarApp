34957e976bdc414d3af17731ff02f1261acf0223
Madalina294 <puicanmadalinamaria@gmail.com>
2025-08-11 22:06:17 +0300

    Created a dynamic navbar for users

--- sellCar_angular/src/app/app.component.ts
+++ sellCar_angular/src/app/app.component.ts
@@ -1,16 +1,41 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NzHeaderComponent} from 'ng-zorro-antd/layout';
 import {NzRowDirective} from 'ng-zorro-antd/grid';
 import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {StorageService} from './auth/services/storage/storage.service';
+import {NgIf} from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NzHeaderComponent, NzRowDirective, RouterLink, NzButtonComponent, RouterLinkActive, NzButtonComponent],
+  imports: [RouterOutlet, NzHeaderComponent, NzRowDirective, RouterLink, NzButtonComponent, RouterLinkActive, NzButtonComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'sellCar_angular';
+
+  constructor(private router: Router) {
+  }
+
+  isAdminLoggedIn: boolean = false;
+  isCustomerLoggedIn: boolean = false;
+
+  ngOnInit(){
+    this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+    this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+    this.router.events.subscribe(event => {
+      if(event.constructor.name === "NavigationEnd"){
+        this.isAdminLoggedIn = StorageService.isAdminLoggedIn();
+        this.isCustomerLoggedIn = StorageService.isCustomerLoggedIn();
+      }
+    })
+  }
+
+
+  logout() {
+    StorageService.signout();
+    this.router.navigateByUrl("/login");
+  }
 }
