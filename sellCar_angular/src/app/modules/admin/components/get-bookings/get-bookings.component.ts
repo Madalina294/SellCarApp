ac5d69c06424ff091a345edb48bf615f20d4c250
Madalina294 <puicanmadalinamaria@gmail.com>
2025-08-19 17:55:48 +0300

    Added View All Deals feature for Admin

--- sellCar_angular/src/app/modules/admin/components/get-bookings/get-bookings.component.ts
+++ sellCar_angular/src/app/modules/admin/components/get-bookings/get-bookings.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {CustomerService} from '../../../customer/services/customer.service';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {AdminService} from '../../services/admin.service';
+import {NgClass, NgForOf} from '@angular/common';
+import {NzTableComponent} from 'ng-zorro-antd/table';
+import {NzSpinComponent} from 'ng-zorro-antd/spin';
 
 @Component({
   selector: 'app-get-bookings',
-  imports: [],
+  imports: [
+    NgClass,
+    NgForOf,
+    NzTableComponent,
+    NzSpinComponent
+  ],
   templateUrl: './get-bookings.component.html',
   styleUrl: './get-bookings.component.scss'
 })
 export class GetBookingsComponent {
+  bids: any = [];
+  issSpinning: boolean = false;
+  constructor(private service: AdminService,
+              private message: NzMessageService) {
+  }
 
+  ngOnInit(){
+    this.getBids();
+  }
+
+  getBids(){
+    this.issSpinning = true;
+    this.service.getAllBids().subscribe((res)=>{
+      this.issSpinning = false;
+      console.log(res);
+      this.bids = res;
+    })
+  }
+
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
+  }
 }
--- sellCar_angular/src/app/modules/admin/services/admin.service.ts
+++ sellCar_angular/src/app/modules/admin/services/admin.service.ts
@@ -17,6 +17,12 @@ export class AdminService {
     });
   }
 
+  getAllBids(): Observable<any>{
+    return this.http.get(BASE_URL + "api/admin/car/bids", {
+      headers: this.createAuthorizationHeader()
+    });
+  }
+
   createAuthorizationHeader(): HttpHeaders{
     let authHeaders: HttpHeaders = new HttpHeaders();
     return authHeaders.set(
